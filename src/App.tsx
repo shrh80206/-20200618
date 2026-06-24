96525d86dac366c6885c1d62a6358a0d7c696115
shrh80206 <161785774+shrh80206@users.noreply.github.com>
2026-06-24 23:09:26 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import Game from './components/Game'
+// 把第 3 行改成帶大括號的導入
+import { Game } from './components/Game'
 import './App.css'
 
 interface Record {
